88e96d9007896b5e5e94feeffded27479c3d1032
ksaric <kristijan.saric@iohk.io>
2020-10-29 12:19:27 +0100

    [CAD-1356] Add tests for the existing functionality.

--- test/SmashSpec.hs
+++ test/SmashSpec.hs
@@ -24,93 +24,124 @@ smashSpec :: Spec
 smashSpec = do
     describe "DataLayer" $ do
         describe "Delisted pool" $
-            it "should test these things in comments" $ pending
---            prop "adding a pool hash adds it to the data layer" $ monadicIO $ do
---
---                (pk, _)             <- run $ createKeypair
---
---                let newPoolHash :: PoolMetadataHash
---                    newPoolHash = PoolMetadataHash . show $ pk
---
---                let blacklistedPoolHash :: PoolMetadataHash
---                    blacklistedPoolHash = PoolMetadataHash $ getPoolMetadataHash newPoolHash
---
---                ioDataMap           <- run $ newIORef stubbedInitialDataMap
---                ioBlacklistedPools  <- run $ newIORef stubbedBlacklistedPools
---
---                let dataLayer :: DataLayer
---                    dataLayer = stubbedDataLayer ioDataMap ioBlacklistedPools
---
---                newBlacklistPoolState <- run $ (dlAddBlacklistedPool dataLayer) blacklistedPoolHash
---
---                isBlacklisted <- run $ (dlCheckBlacklistedPool dataLayer) blacklistedPoolHash
---
---                assert $ isRight newBlacklistPoolState
---                assert $ isBlacklisted
---
---        describe "Pool metadata" $ do
---            prop "adding a pool metadata and returning the same" $ \(poolOfflineMetadata) -> monadicIO $ do
---
---                (pk, _)             <- run $ createKeypair
---
---                let newPoolHash :: PoolMetadataHash
---                    newPoolHash = PoolMetadataHash . show $ pk
---
---                ioDataMap           <- run $ newIORef stubbedInitialDataMap
---                ioBlacklistedPools  <- run $ newIORef stubbedBlacklistedPools
---
---                let dataLayer :: DataLayer
---                    dataLayer = stubbedDataLayer ioDataMap ioBlacklistedPools
---
---                newPoolOfflineMetadata  <- run $ (dlAddPoolMetadata dataLayer) newPoolHash poolOfflineMetadata
---
---                newPoolOfflineMetadata' <- run $ (dlGetPoolMetadata dataLayer) newPoolHash
---
---                assert $ isRight newPoolOfflineMetadata
---                assert $ isRight newPoolOfflineMetadata'
---
---                assert $ newPoolOfflineMetadata == newPoolOfflineMetadata'
---
---            prop "query non-existing pool metadata" $ monadicIO $ do
---
---                (pk, _)             <- run $ createKeypair
---
---                let newPoolHash :: PoolMetadataHash
---                    newPoolHash = PoolMetadataHash . show $ pk
---
---                ioDataMap           <- run $ newIORef stubbedInitialDataMap
---                ioBlacklistedPools  <- run $ newIORef stubbedBlacklistedPools
---
---                let dataLayer :: DataLayer
---                    dataLayer = stubbedDataLayer ioDataMap ioBlacklistedPools
---
---                newPoolOfflineMetadata <- run $ (dlGetPoolMetadata dataLayer) newPoolHash
---
---                -- This pool hash does not exist!
---                assert $ isLeft newPoolOfflineMetadata
---
---
---genSafeChar :: Gen Char
---genSafeChar = elements ['a'..'z']
---
---genSafeText :: Gen Text
---genSafeText = toS <$> listOf genSafeChar
---
----- TODO(KS): Create more realistic arbitrary instance.
---instance Arbitrary Text where
---    arbitrary = genSafeText
---
---instance Arbitrary PoolOfflineMetadata where
---    arbitrary = do
---        poolName        <- PoolName         <$> genSafeText
---        poolDescription <- PoolDescription  <$> genSafeText
---        poolTicker      <- PoolTicker       <$> genSafeText
---        poolHomepage    <- PoolHomepage     <$> genSafeText
---
---        return $ createPoolOfflineMetadata
---            poolName
---            poolDescription
---            poolTicker
---            poolHomepage
---
+            prop "adding a pool hash adds it to the data layer" $ monadicIO $ do
+
+                (pk, _) <- run $ createKeypair
+
+                let newPoolId :: PoolId
+                    newPoolId = PoolId . show $ pk
+
+                let delistedPoolId :: PoolId
+                    delistedPoolId = PoolId $ getPoolId newPoolId
+
+                dataLayer <- run createStubbedDataLayer
+
+                let addDelistedPool = dlAddDelistedPool dataLayer
+                newDelistedPoolState <- run $ addDelistedPool delistedPoolId
+
+                let checkDelistedPool = dlCheckDelistedPool dataLayer
+                isDelisted <- run $ checkDelistedPool delistedPoolId
+
+                assert $ isRight newDelistedPoolState
+                assert $ isDelisted
+
+        describe "Fetch errors" $
+            prop "adding a fetch error adds it to the data layer" $ monadicIO $ do
+
+                (pk, _) <- run $ createKeypair
+
+                let newPoolId :: PoolId
+                    newPoolId = PoolId . show $ pk
+
+                dataLayer <- run createStubbedDataLayer
+
+                let addRetiredPool = dlAddRetiredPool dataLayer
+                retiredPoolId <- run $ addRetiredPool newPoolId
+
+                let getRetiredPools = dlGetRetiredPools dataLayer
+                retiredPoolsId <- run $ getRetiredPools
+
+                assert $ isRight retiredPoolId
+                assert $ isRight retiredPoolsId
+
+                let retiredPoolIdE      = fromRight (PoolId "X") retiredPoolId
+                let retiredPoolsIdE     = fromRight [] retiredPoolsId
+
+                assert $ retiredPoolIdE `elem` retiredPoolsIdE
+
+
+        describe "Pool metadata" $ do
+            prop "adding a pool metadata and returning the same" $ \(poolOfflineMetadata) -> monadicIO $ do
+
+                (pk, _) <- run $ createKeypair
+
+                let newPoolId :: PoolId
+                    newPoolId = PoolId . show $ pk
+
+                let newPoolHash :: PoolMetadataHash
+                    newPoolHash = PoolMetadataHash . show $ pk
+
+                -- At some point we might switch to using actual pool metadata for testing
+                --poolOfflineMetadata <- run $ readFile "./test_pool.json"
+
+                let poolTicker :: PoolTicker
+                    poolTicker = PoolTicker "TESTT"
+
+                dataLayer <- run createStubbedDataLayer
+
+                -- Maybe PoolMetadataReferenceId -> PoolId -> PoolMetadataHash -> Text -> PoolTicker -> IO (Either DBFail Text)
+                let addPoolMetadata = dlAddPoolMetadata dataLayer
+                newPoolOfflineMetadata  <- run $ addPoolMetadata Nothing newPoolId newPoolHash poolOfflineMetadata poolTicker
+
+                let getPoolMetadata = dlGetPoolMetadata dataLayer
+                newPoolOfflineMetadata' <- run $ getPoolMetadata newPoolId newPoolHash
+
+                assert $ isRight newPoolOfflineMetadata
+                assert $ isRight newPoolOfflineMetadata'
+
+                --assert $ newPoolOfflineMetadata == newPoolOfflineMetadata'
+
+            prop "query non-existing pool metadata" $ monadicIO $ do
+
+                (pk, _)             <- run $ createKeypair
+
+                let newPoolId :: PoolId
+                    newPoolId = PoolId . show $ pk
+
+                let newPoolHash :: PoolMetadataHash
+                    newPoolHash = PoolMetadataHash . show $ pk
+
+                dataLayer <- run createStubbedDataLayer
+
+                let getPoolMetadata = dlGetPoolMetadata dataLayer
+                newPoolOfflineMetadata <- run $ getPoolMetadata newPoolId newPoolHash
+
+                -- This pool hash does not exist!
+                assert $ isLeft newPoolOfflineMetadata
+
+
+genSafeChar :: Gen Char
+genSafeChar = elements ['a'..'z']
+
+genSafeText :: Gen Text
+genSafeText = toS <$> listOf genSafeChar
+
+-- TODO(KS): Generate realistic @PoolId@ and @PoolMetadataHash@ values.
+
+-- TODO(KS): Create more realistic arbitrary instance.
+instance Arbitrary Text where
+    arbitrary = genSafeText
+
+instance Arbitrary PoolOfflineMetadata where
+    arbitrary = do
+        poolName        <- PoolName         <$> genSafeText
+        poolDescription <- PoolDescription  <$> genSafeText
+        poolTicker      <- PoolTicker       <$> genSafeText
+        poolHomepage    <- PoolHomepage     <$> genSafeText
+
+        return $ createPoolOfflineMetadata
+            poolName
+            poolDescription
+            poolTicker
+            poolHomepage
 
